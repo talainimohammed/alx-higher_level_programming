ac4afd392d76b26c1b5a77488a615bbd824a8eaf
Talaini <talainimohammed@gmail.com>
2023-06-06 11:48:39 +0100

    updates

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ nb = int(repr(number)[-1])
 x = "Last digit of "
 if nb > 5:
     print(x + str(number) + " is " + str(nb) + " and is greater than 5")
-elif nb < 6 and nb > 0:
+elif nb < 6 and nb != 0:
     print(x + str(number) + " is " + str(nb) + " and is less than 6 and not 0")
 else:
     print(x + str(number) + " is " + str(nb) + " and is 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 x = ""
 for i in range(97, 123):
-    x += chr(i)
+    x += "{0:c}".format(i)
 print(x)
