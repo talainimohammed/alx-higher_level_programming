eff61a3258697c46280d8f2f746767515951a108
Talaini <talainimohammed@gmail.com>
2023-06-06 21:46:17 +0100

    Create 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+x = ""
+for i in range(0, 9):
+    for j in range(1, 10):
+        if j > i:
+            x += "{0:d}".format(i) + "{0:d}".format(j)
+            if i != 8 or j != 9:
+                 x += ", "
+print(x)
