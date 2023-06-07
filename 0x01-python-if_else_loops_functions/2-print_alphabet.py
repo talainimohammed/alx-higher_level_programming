eb0db608908cf91ce2ef8c44f0025c8b48e3e4bd
Talaini <talainimohammed@gmail.com>
2023-06-07 11:03:37 +0100

    updates

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 x = ""
 for i in range(97, 123):
     x += "{0:c}".format(i)
-print(x, end = ' ')
+print(x, end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ x = ""
 for i in range(97, 123):
     if i != 101 and i != 113:
         x += "{0:c}".format(i)
-print(x, end = ' ')
+print(x, end='')
