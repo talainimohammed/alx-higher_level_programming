df2d079eaa0c8a859896d049019ff00be77dd39f
Talaini <talainimohammed@gmail.com>
2023-06-08 16:10:00 +0100

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,18 +2,19 @@
 import sys
 
 if __name__ != "__main__":
-    str = "{:d} argument"
-    nbarg = len(sys.argv) - 1
-    if nbarg == 0:
-        str += 's.'
-    elif nbarg == 1:
-        str += ':'
-    else:
-        str += 's:'
-    print(str.format(nbarg))
+    exit()
+str = "{:d} argument"
+nbarg = len(sys.argv) - 1
+if nbarg == 0:
+    str += 's.'
+elif nbarg == 1:
+    str += ':'
+else:
+    str += 's:'
+print(str.format(nbarg))
 
-    i = 0
-    for value in sys.argv:
-        if i != 0:
-            print("{:d}: {:s}".format(i, value))
-        i += 1
+i = 0
+for value in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, value))
+    i += 1
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,10 @@ if __name__ != "__main__":
     exit()
 
 i = 0
-result = 0
+res = 0
 for argvalue in sys.argv:
     if i != 0:
-        result += int(argvalue)
+        res += int(argvalue)
     else:
         i += 1
-print("{:d}".format(result))
+print("{:d}".format(res))
