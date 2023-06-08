a1c67979717a4c0c4af587bb6d1f4fddddf747d5
Talaini <talainimohammed@gmail.com>
2023-06-08 15:59:08 +0100

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+str = "{:d} argument"
+nbarg = len(sys.argv) - 1
+if nbarg == 0:
+    str += 's.'
+elif nbarg == 1:
+    str += ':'
+else:
+    str += 's:'
+print(str.format(nbarg))
+
+i = 0
+for value in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, value))
+    i += 1
