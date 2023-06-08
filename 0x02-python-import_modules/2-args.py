6992f3e04d0736d26765b7e103ccccc3ada9a4da
Talaini <talainimohammed@gmail.com>
2023-06-08 16:03:32 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,20 +2,18 @@
 import sys
 
 if __name__ != "__main__":
-    exit()
+    str = "{:d} argument"
+    nbarg = len(sys.argv) - 1
+    if nbarg == 0:
+        str += 's.'
+    elif nbarg == 1:
+        str += ':'
+    else:
+        str += 's:'
+    print(str.format(nbarg))
 
-str = "{:d} argument"
-nbarg = len(sys.argv) - 1
-if nbarg == 0:
-    str += 's.'
-elif nbarg == 1:
-    str += ':'
-else:
-    str += 's:'
-print(str.format(nbarg))
-
-i = 0
-for value in sys.argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, value))
-    i += 1
+    i = 0
+    for value in sys.argv:
+        if i != 0:
+            print("{:d}: {:s}".format(i, value))
+        i += 1
