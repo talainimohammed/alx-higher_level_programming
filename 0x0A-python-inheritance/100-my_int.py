caed3e0c5f25bb83d55ad74d6a247e1758766f15
Talaini <talainimohammed@gmail.com>
2023-07-10 18:01:08 +0100

    updates

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""No Module"""
+
+class MyInt(int):
+    """inversed int, overriding operator methods
+    """
+
+    def __eq__(self, other):
+        return (int(self) != other)
+
+    def __ne__(self, other):
+        return (int(self) == other)
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""No Module"""
+
+def add_attribute(obj, name, value):
+    """adds an attribute to the class
+    """
+    builtins = (str, int, complex, bool, float, list,
+                tuple, dict, set, frozenset, type, object)
+    for _type in builtins:
+        if type(obj) is _type:
+            raise TypeError("can't add new attribute")
+
+    object.__setattr__(obj, name, value)
