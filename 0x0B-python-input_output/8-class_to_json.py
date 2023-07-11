2568b413d822a429c111a361fafc9aa6f2f74d23
Talaini <talainimohammed@gmail.com>
2023-07-11 16:08:12 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,4 +6,4 @@ def class_to_json(obj):
     """creates a json representation
     """
     return {key: value for (key, value) in obj.__dict__.items()
-                if key in list(obj.__dict__.keys())}
+            if key in list(obj.__dict__.keys())}
