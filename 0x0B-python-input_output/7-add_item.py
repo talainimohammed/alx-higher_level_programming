a15664c58f1dc1e4c2b423281e502d5c05f9a5a4
Talaini <talainimohammed@gmail.com>
2023-07-11 15:57:39 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -16,7 +16,7 @@ def main():
     except:
         new_l = []
 
-    new_l.extend([sys.argv[i] for j in range(0, len(sys.argv)) if j != 0])
+    new_l.extend([sys.argv[j] for j in range(0, len(sys.argv)) if j != 0])
     try:
         save_to_json_file(new_l, 'add_item.json')
     except:
